0a3250dc17f69e97fb408c38c6f44c674dfd4b3a
asifahmed1810 <asifahmed18oct@gmail.com>
2025-02-23 00:02:19 +0600

    Update login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+
+
 
 const Login = () => {
+
+  const{googleSignIn,user}=useContext(AuthContext);
+
+  const handleLogin = () => {
+    googleSignIn()
+      .then((result) => {
+        console.log("User: ", result.user);
+      })
+      .catch((error) => {
+        console.error("Login Error:", error.message);
+      });
+  };
+  
+
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
         <h2 className="text-2xl font-semibold text-center mb-6">Login</h2>
         
         <div className="mt-4 text-center">
-          <button className="w-full btn btn-neutral flex items-center justify-center gap-2 bg-base-200 text-black py-2 rounded-lg hover:bg-red-600 transition">
+          <button onClick={handleLogin} className="w-full btn btn-neutral flex items-center justify-center gap-2 bg-base-200 text-black py-2 rounded-lg hover:bg-red-600 transition">
             <img
               src="https://www.svgrepo.com/show/475656/google-color.svg"
               alt="Google Logo"
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -4,17 +4,22 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from '../firebase/firebase.init';
 
 
+
 export const AuthContext = createContext(null);
-const auth=getAuth(app);
+
 const googleProvider=new GoogleAuthProvider();
+const auth=getAuth(app)
 const AuthProvider = ({children}) => {
     const [user,setUser]=useState(null);
     const googleSignIn=()=>{
         return signInWithPopup(auth,googleProvider);
     }
     const authInfo={
+        user,
         googleSignIn
     }
+
+
     return <AuthContext.Provider value={authInfo}>
        {children}
     </AuthContext.Provider>
